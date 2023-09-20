528a92ae95d95608749ad695722e120c589950d4
Vitalina03 <127879557+Vitalina03@users.noreply.github.com>
2023-09-20 16:13:11 +0300

    ytuy
    114w5

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Wrapper, Button } from './feedbackOptions.styled';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <Wrapper>
+      {options.map(option => (
+        <Button
+          key={option}
+          type="button"
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </Button>
+      ))}
+    </Wrapper>
+  );
+};
